589f83f0f06691e6c94731afa4626b5060557943
翁加栋 <wengjiadong@wengjiadongdeMacBook-Pro.local>
2018-07-19 00:03:01 +0900

    update

--- factorial.js
+++ factorial.js
@@ -10,5 +10,13 @@ const factorial_normal = n => {
   }
   return n * factorial_normal(n - 1)
 };
+const factorial_loop = (n) => {
+  let ret = 1;
+  for(;n > 1;n--){
+    ret *=n;
+  }
+  return ret;
+}
 console.log(factorial(8955)); // RangeError: Maximum call stack size exceeded
 console.log(factorial_normal(8960)); // Infinity
+console.log(factorial_loop(100000000)); // Infinity
