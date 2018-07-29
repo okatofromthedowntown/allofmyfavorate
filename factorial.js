c26a5ea0b626a484af4e1de4f706973e29024d46
翁加栋 <wengjiadong@wengjiadongdeMacBook-Pro.local>
2018-07-29 17:46:59 +0900

    update

--- factorial.js
+++ factorial.js
@@ -17,6 +17,22 @@ const factorial_loop = (n) => {
   }
   return ret;
 }
-console.log(factorial(8955)); // RangeError: Maximum call stack size exceeded
-console.log(factorial_normal(8960)); // Infinity
-console.log(factorial_loop(100000000)); // Infinity
+// console.log(factorial(8955)); // RangeError: Maximum call stack size exceeded
+// console.log(factorial_normal(8960)); // Infinity
+// console.log(factorial_loop(100000000)); // Infinity
+
+const fibonacci_normal = (n) => {
+  if(n == 0 || n == 1) return n;
+  return fibonacci_normal(n - 1) + fibonacci_normal(n - 2);
+}
+// console.log(fibonacci_normal(6));
+// console.log(fibonacci_normal(7));
+// console.log(fibonacci_normal(8));
+
+const fibonacci = (n, a1 = 1, a2 = 1) => {
+  if (n < 2) return a2;
+  return fibonacci((n - 1), a2, a1 + a2 );
+}
+// console.log(fibonacci(6));
+// console.log(fibonacci(7));
+// console.log(fibonacci(8));
